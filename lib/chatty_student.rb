1907318dda3dd68abfd6cdc30e50e673ffb00422
Grace Kim <goooyoon@gmail.com>
2020-06-09 19:27:41 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,15 +6,7 @@ class ChattyStudent
   end
   
   def raise_hand
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
+  10 times.do
     super
   end
 end
